c8324476d4734acc49eefcd27f979d2d87090530
Victoria Wagner Trento <victoriaawagnertrento@gmail.com>
2023-02-18 01:40:16 +0100

    Creando componentes

---
+++ client/src/components/Home/Home.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+
+// HOME PAGE | la página principal de tu SPA debe contener:
+
+// SearchBar: un input de búsqueda para encontrar países por nombre.
+// Sector en el que se vea un listado de cards con los países. Al iniciar deberá cargar los 
+//primeros resultados obtenidos desde la ruta GET /countries y deberá mostrar su:
+// Imagen de la bandera.
+// Nombre.
+// Continente.
+// Cuando se le hace click a una Card deberá redirigir al detalle de ese país específico.
+// Botones/Opciones para filtrar por continente y por tipo de actividad turística.
+// Botones/Opciones para ordenar tanto ascendentemente como descendentemente los países por orden alfabético
+// y por cantidad de población.
+// Paginado: el listado de países se hará por partes. Tu SPA debe contar con un paginado que muestre un 
+//total de 10 países por página.
+
+
+const Home = () => {
+   return(
+      <div>
+
+      </div>
+   )
+
+}
+
+export default Home;
---
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+
+
+
+// LANDING PAGE | deberás crear una página de inicio o bienvenida con:
+
+// Alguna imagen de fondo representativa al proyecto.
+// Botón para ingresar a la home page.
+
+
+
+const LandingPage = () => {
+   return (
+   	<div>
+         <h1>Welcome to my website about countries.</h1>
+      </div>
+
+	)
+}
+
+export default LandingPage;
