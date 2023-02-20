25b20724e1bb1eaba0ddb2096f29dd36deaf8b41
Victoria Wagner Trento <victoriaawagnertrento@gmail.com>
2023-02-20 19:50:58 +0100

    Update

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,12 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 
-const Card = (props) => { 
+function Card ({ name, continent, image }) { 
     return(
         <div>
-
+             <Link to={`/detail/{}`} ></Link>
+            <h3>Name: {name}</h3>
+            <p>Continent: {continent}</p>
+            <br />
+            <img src={image} alt={name} />
         </div>
-    )
-    }
+    );
+}
 
-export default Card;
+export default Card; 
