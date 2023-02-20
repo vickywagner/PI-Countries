20e2dcdb5609eebd1f5427cde8080c4aa3bb0f5e
Victoria Wagner Trento <victoriaawagnertrento@gmail.com>
2023-02-20 19:51:49 +0100

    Renderizando paises

--- client/src/components/Cards/Cards.jsx
+++ client/src/components/Cards/Cards.jsx
@@ -1,24 +1,35 @@
-import React from "react";
+import React , {useEffect} from "react";
+import { useSelector } from "react-redux";
 import Card from "../Card/Card";
+import { useDispatch } from "react-redux";
+import { getCountries } from "../../redux/actions";
 
 
-// {countries.map(({id, name, continent, capital, subregion, area, population, image }) => {
-//     return <Card 
-//     key={id}
-//     name={name}
-//     continent={continent}
-//     capital={capital}
-//     subregion={subregion}
-//     area={area}
-//     population={population}
-//     image={image}
-//     id = {id}
-//     />
-// })}
+//ver si esto lo hacemos directo en el home
+
 const Cards = () => { 
+    const dispatch = useDispatch();
 
+    useEffect(() => {  
+        dispatch(getCountries());
+    },[dispatch])
+
+    const countries = useSelector(state => state.countries);
     return(
         <div>
+            {countries?.map(({id, name, continent, capital, subregion, area, population, image }) => {
+                return <Card 
+                key={id}
+                name={name}
+                continent={continent}
+                capital={capital}
+                subregion={subregion}
+                area={area}
+                population={population}
+                image={image}
+                id = {id}
+                />
+            })}
         </div>
     )
     }
