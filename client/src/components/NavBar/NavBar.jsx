77d7b0de0ad2ee8d8c8abe3ebdf8cc9abd232edd
Victoria Wagner Trento <victoriaawagnertrento@gmail.com>
2023-02-20 19:55:27 +0100

    Nav

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import style from "../NavBar/NavBar.module.css"
-import { Link } from "react-router-dom";
+import SearchBar from "../SearchBar/SearchBar";
+import { Link, useLocation } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { getCountries } from "../../redux/actions";
 
-const NavBar = () => {
+const NavBar = ({onSearch}) => {
+    const [visibility, setVisibility] = useState(null) // para que se muestre el nav en todas menos en landing
+    const dispatch = useDispatch();  // Nos traemos los paises
+    const location = useLocation();
+
+    useEffect(() => {  // ya traemos todos los paises para cuando abrimos el home ya se cargan y el usuario no tiene que esperar
+        dispatch(getCountries());
+    },[dispatch])
+
+    useEffect(() => {
+       setVisibility(location.pathname === '/' ? "none" : null)
+       //console.log("Funcionando")
+    },[location.pathname])
+
     return(
-        <div className={style.mainContainer}>
-            <Link to='/home'>HOME</Link>
-            <Link to='/create' className={style.home}>FORM</Link>
+       <nav className={style.nav}>
+       <div className={style.mainContainer}  style={{display: visibility}} >
+            <Link to='/home' className={style.links}>HOME</Link>
+            <Link to='/create' className={style.links}>FORM</Link>
+            <SearchBar onSearch={onSearch} />
             </div>
+       </nav>
 
 // VER SI HACEMOS FAV <Link to='/favorites' className={style.about} >Favorites</Link>
     )
