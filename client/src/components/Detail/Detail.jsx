c0a6fd0a51147d9a78a06d5580d6cbef9fa9b74f
Victoria Wagner Trento <victoriaawagnertrento@gmail.com>
2023-02-22 02:57:16 +0100

    Funciona-Faltan estilos

--- client/src/components/Detail/Detail.jsx
+++ client/src/components/Detail/Detail.jsx
@@ -1,9 +1,12 @@
 import React from "react";
-
+import { getDetail } from "../../redux/actions";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useParams, Link } from "react-router-dom";
 
 // DETAIL PAGE | en esta vista se deberá mostrar toda la información específica de un país:
 
-// ID (Código de tres letras).
+// ID (Código de tres letrass).
 // Nombre.
 // Imagen de la bandera.
 // Continente.
@@ -14,11 +17,30 @@ import React from "react";
 
 
 const Detail = () => { 
+    const dispatch = useDispatch();
+    const { id } = useParams(); //me traigo el id desde la ruta gracias al useParams
+    const countryDetail = useSelector((state) => state.detail)
+
+    useEffect(() => {
+        dispatch(getDetail(id))
+    },[])
+
     return(
         <div>
-
+            <img src={countryDetail?.image} alt={countryDetail?.name} />
+            <h2>{countryDetail?.name}</h2>
+            <h3>{countryDetail?.id}</h3>
+            <p>Continent: {countryDetail?.continent}</p>
+            <p>Capital: {countryDetail?.capital}</p>
+            <p>Subregion: {countryDetail?.subregion}</p>
+            <p>Area: {countryDetail?.area}km2</p>
+            <p>Population: {countryDetail?.population}</p>
+            
+            <button>
+                <Link to='/home'>Back to Home</Link>
+            </button>
         </div>
     )
-    }
+}
 
 export default Detail;
