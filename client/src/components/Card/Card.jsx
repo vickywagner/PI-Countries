100a5a5f6bf8bbc72f76126b7a86188178787346
Victoria Wagner Trento <victoriaawagnertrento@gmail.com>
2023-02-22 02:57:54 +0100

    Funciona-Falta estilos

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 
-function Card ({ name, continent, image }) { 
+function Card ({ id, name, continent, image }) { 
     return(
         <div>
-             <Link to={`/detail/{}`} ></Link>
             <h3>Name: {name}</h3>
             <p>Continent: {continent}</p>
-            <br />
-            <img src={image} alt={name} />
+             <Link to={`/detail/${id}`} >
+                <img src={image} alt={name} />
+             </Link>
         </div>
     );
 }
