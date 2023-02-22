456f4d5cc85b07a765dd3b295c5165bb4477369b
Victoria Wagner Trento <victoriaawagnertrento@gmail.com>
2023-02-22 02:49:38 +0100

    Anda con algunos bugs

--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -1,22 +1,37 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { searchByName } from "../../redux/actions";
 import style from "../SearchBar/SearchBar.module.css"
 
-const SearchBar = ({ onSearch}) => {
-    const [country, setCountry] = useState('') // mi estado es un string no un obj
-    //character es el estado
-    
-       const handleChange = (event) => {
-        setCountry(event.target.value)
-    //mi estado pasa a ser el event.target.value
-       }
+const SearchBar = () => {
+    const dispatch = useDispatch();
+    const [name, setName] = useState(''); // seteo mi estado como string
+
+    const handleInputChange = (event) => {
+        event.preventDefault();
+        setName(event.target.value); //mi estado pasa a ser el event.target.value
+        //console.log(name) 
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        dispatch(searchByName(name));
+    }
+
     return(
         <div className ={style.container}>
-            <input className ={style.input} type='search' value={country} onChange={handleChange} /> 
-            <button className ={style.btn} onClick={() => onSearch(country)}>Buscar</button> 
+            <input className ={style.input}
+                id="search"
+                type='text'
+                placeholder="Search..."
+                onChange={(event) => handleInputChange(event)} /> 
+            
+            <button className ={style.btn}
+                 type='submit'
+                 onClick={(event) => handleSubmit(event)} >Search</button> 
       </div>
     )
-    
+
 }
 
 export default  SearchBar;
