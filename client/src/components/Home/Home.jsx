4e861829c8a2ac6c1a95400766071c5e6a2fa69a
Victoria Wagner Trento <victoriaawagnertrento@gmail.com>
2023-02-22 02:51:59 +0100

    Update

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,52 +1,81 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import { useDispatch, useSelector, UseSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { getCountries } from "../../redux/actions";
 //import { Link } from "react-router-dom";
-import Cards from "../Cards/Cards";
+import Card from "../Card/Card";
+import Filters from "../Filters/Filters";
+import Pagination from "../Pagination/Pagination";
 
 // HOME PAGE | la página principal de tu SPA debe contener:
 
 // SearchBar: un input de búsqueda para encontrar países por nombre.
-// Sector en el que se vea un listado de cards con los países. Al iniciar deberá cargar los 
-//primeros resultados obtenidos desde la ruta GET /countries y deberá mostrar su:
-// Imagen de la bandera.
-// Nombre.
-// Continente.
-// Cuando se le hace click a una Card deberá redirigir al detalle de ese país específico.
+      // Sector en el que se vea un listado de cards con los países. Al iniciar deberá cargar los 
+      //primeros resultados obtenidos desde la ruta GET /countries y deberá mostrar su:
+      // Imagen de la bandera.
+      // Nombre.
+      // Continente.
+      // Cuando se le hace click a una Card deberá redirigir al detalle de ese país específico.
 // Botones/Opciones para filtrar por continente y por tipo de actividad turística.
 // Botones/Opciones para ordenar tanto ascendentemente como descendentemente los países por orden alfabético
 // y por cantidad de población.
 // Paginado: el listado de países se hará por partes. Tu SPA debe contar con un paginado que muestre un 
 //total de 10 países por página.
-
-
+ 
 const Home = () => {
-
-   // const dispatch = useDispatch()
-   // const allCountries = useSelector((state) => state.countries)
-
-   // useEffect(() => {
-   //    dispatch(getCountries());
-   // },[])
-
-
-   // const handleClick = (event)=> {
-   //    event.preventDefault();
-   //    dispatch(getCountries());
-   // }
-
-   // <Link to= '/activities'>Create activity</Link>
-   // boton dentro del return <button onSubmit={handleClick} >Reload Countries</button>
    
+   const dispatch = useDispatch();
+   const allCountries = useSelector((state) => state.countries) // traemos lo q esta en el estado de countries
+   
+   //********* PAGINADO ***********
+   const [currentPage, setcurrentPage] = useState(1) // guardamos en el estado local la pag actual, empieza en 1 xq siempre arrancamos de la pag 1
+   const [countriesPerPage, setcountriesPerPage] = useState(10) // guardamos en el estado local lacant de paises que queremos mostrar, son 10 paises x pag
+   const indexOfLastCountry = currentPage * countriesPerPage // mi pag * paisesxpag = 10
+   const indexOfFirstCountry = indexOfLastCountry - countriesPerPage // 0
+   const currentCountries = allCountries.slice(indexOfFirstCountry,indexOfLastCountry) //los paises que estan la pag actual
+
+   const paginado = (pageNumber) => {
+      setcurrentPage(pageNumber)
+   }
+//**************FILTRO***************/
+const [orden, setOrden] = useState('');
+
+
+// ****** Boton reload countries ******
+  const handleClick = (event)=> {
+     event.preventDefault();
+     dispatch(getCountries());
+   }
+
    return(
       <div>
-         <h1>Country</h1>
-         <Cards />
-         <div></div>
+         <h1>Countries</h1>
+        {/*  <Link to= '/activities'>Create activity</Link>
+         */}
+         <button onClick={handleClick} >Reload Countries</button> 
+        <Filters />
+        <Pagination countriesPerPage={countriesPerPage} 
+            allCountries={allCountries.length}
+            paginado={paginado}/>
+         
+         {currentCountries?.map((c) => {
+                return (
+                <div>
+                  <Card 
+                     image={c.image}
+                     key={c.id}
+                     name={c.name}
+                     continent={c.continent}
+                     capital={c.capital}
+                     subregion={c.subregion}
+                     area={c.area}
+                     population={c.population}
+                     id = {c.id}/>
+                </div>
+                  )})
+            }
       </div>
    )
-
 }
 
 export default Home;
